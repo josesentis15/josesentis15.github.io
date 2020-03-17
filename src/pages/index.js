7fdbf79567bc5e53f1d351e61907e7609958dcc5
Jose <jose@runroom.com>
2020-03-17 20:31:26 +0100

    Hides content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
 
 import Layout, { Wrapper } from '../components/layout';
-import Navigation from '../components/navigation';
-// import NavigationWrapper from '../components/navigation/styles';
+// import Navigation from '../components/navigation';
+import NavigationWrapper from '../components/navigation/styles';
 
 class Home extends React.Component {
   render() {
     return (
       <Layout location={this.props.location} className="home">
         <Wrapper>
-          <Navigation />
-          {/* <NavigationWrapper>
+          {/* <Navigation /> */}
+          <NavigationWrapper>
             <Link to={`/`} className="title link">New site coming soon...</Link>
-          </NavigationWrapper> */}
+          </NavigationWrapper>
         </Wrapper>
       </Layout>
     );
