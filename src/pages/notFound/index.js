82e58877d64a79681077150d21acc028ae6f393a
Jose <jose@runroom.com>
2020-04-04 12:16:46 +0200

    Adds 404 animation with p5

---
+++ src/pages/notFound/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from "react-router";
+import Sketch from "react-p5";
+
+import Glitch from '../../components/glitch';
+import Layout, { Wrapper } from '../../components/layout';
+
+import routes from '../../utils/routes';
+import image from './404.png';
+
+class NotFound extends React.Component {
+  _img;
+  _text = 'Error 404';
+
+  preload = p5 => {
+    this._img = p5.loadImage(image);
+  }
+
+  setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+  }
+
+  draw = p5 => {
+    p5.image(this._img, p5.mouseX, p5.mouseY);
+  }
+
+  render() {
+    return (
+      <Layout
+        className="layout-bottom"
+        location={this.props.location}
+        title="404: Not Found"
+      >
+        <Sketch setup={this.setup} draw={this.draw} preload={this.preload} />
+        <Wrapper>
+          <h1 className="title">
+            <Glitch className="u-hide--s768" text={this._text} />
+            <Glitch className="u-hide--s768" text={this._text} />
+            <Glitch text={this._text} />
+          </h1>
+          <Link to={routes.home}>Get back</Link>
+        </Wrapper>
+      </Layout>
+    );
+  }
+}
+
+export default withRouter(NotFound);
--- src/styles/index.js
+++ src/styles/index.js
@@ -149,6 +149,16 @@ const GlobalStyle = createGlobalStyle`
 
   ${typographyStyles}
   ${utils}
+
+  .react-p5 {
+    display: none;
+    height: 100%;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 0;
+  }
 `;
 
 export default GlobalStyle;
