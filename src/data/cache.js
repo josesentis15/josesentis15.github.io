edf6d62fcec68740043ec0269a0fd0b1e0e1e66c
Jose <jose@runroom.com>
2020-04-22 23:21:40 +0200

    Adds abstract and image for runroom case

--- src/data/cache.js
+++ src/data/cache.js
@@ -57,10 +57,10 @@ const data = {
           title: 'Runroom',
           external: true,
           externalLink: 'https://www.runroom.com/',
-          image: '/images/esade-exed.jpg',
+          image: '/images/covers/runroom.jpg',
           order: '1',
           slug: '',
-          abstract: 'Repositioning Runroom as a reference point within the field of technology and giving birth to a new digital agency, one that looks like what it offers. New branding via Folch.'
+          abstract: `Designed in-house with the help of Folch, on an art direction role, the website was presented as the launching of the new Runroom brand. This new branding repositions Runroom as a reference point in the creative market giving visibility to the real capacities of the company. <br> The main concept is a blurry mixture of colours which was translated into the main animation of the site's home page. This animation was created in a workshop with Edu Prats where using WebGL and shaders the desired look and feel was achieved and integrated into the branding.`
         },
         {
           __typename: 'Project',
