986915249589abb04444ec4f9bdccfbb0ea5b1be
Jose <jose@runroom.com>
2020-04-27 20:18:13 +0200

    Adds scroll to 0 when changing location

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,6 +1,7 @@
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
+
 const isNewLocation = location => {
   const pathChanged = prevLocation.pathname !== location.pathname;
   const hashChanged = prevLocation.hash !== location.hash;
@@ -12,7 +13,10 @@ let prevLocation = {
   pathname: ''
 };
 
-history.listen(location => {
+history.listen((location) => {
+  const isNew = isNewLocation(location);
+
+  if (isNew) window.scrollTo(0, 0);
   prevLocation = location;
 });
 
