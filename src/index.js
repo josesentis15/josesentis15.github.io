9410ac57d97567d933e84ea458442c5d31b593f8
Jose <jose@runroom.com>
2020-05-03 18:40:58 +0200

    Removes coming soon message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ComingSoon from './components/comingSoon';
-// import App from './components/app';
+import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<ComingSoon />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
