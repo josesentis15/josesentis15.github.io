32fedd9ac42dd074eabacaf4e902b0fc97a82c6f
Jose <jose@runroom.com>
2020-05-14 22:05:32 +0200

    Moves js to index.html

--- src/index.js
+++ src/index.js
@@ -7,9 +7,3 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
-
-if (!!window.MSInputMethodContext && !!document.documentMode) {
-  alert('Windows!');
-
-  document.getElementById('explorer-banner').style.display = 'block';
-}
