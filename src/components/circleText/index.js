1ae9ead3be9ef5d6c410501c83bff0756e7f74f5
Jose <jose@runroom.com>
2020-11-27 18:18:08 +0100

    Animates rotation with gsap

--- src/components/circleText/index.js
+++ src/components/circleText/index.js
@@ -1,23 +1,41 @@
 import React from 'react';
-
+import { TweenMax } from 'gsap';
 import TextWrapper, { Character } from './styles';
 
-const circleText = ({ text, ...props }) => {
-  const characters = text.split("");
-  const degrees = 360 / characters.length;
-  let tempDegrees = 0;
+class circleText extends React.Component {
+  state = {
+    scrollSpeed: 0
+  }
 
-  return (
-    <TextWrapper {...props}>
-      <p>
-        {characters.map(char => {
-          tempDegrees += degrees;
+  componentDidMount() {
+    this.animateScroll();
+  };
 
-          return <Character key={`circle-${char}`} rotate={tempDegrees}>{char}</Character>;
-        })}
-      </p>
-    </TextWrapper>
-  );
+  animateScroll = () => {
+    TweenMax.to(this.circleRef, 10, {
+      rotation: '+=360_cw',
+      repeat: 1
+    });
+  };
+
+  render() {
+    const { text, ...props } = this.props;
+    const characters = text.split("");
+    const degrees = 360 / characters.length;
+    let tempDegrees = 0;
+
+    return (
+      <TextWrapper {...props} ref={ref => this.circleRef = ref}>
+        <p>
+          {characters.map(char => {
+            tempDegrees += degrees;
+
+            return <Character key={`circle-${char}`} rotate={tempDegrees}>{char}</Character>;
+          })}
+        </p>
+      </TextWrapper>
+    );
+  }
 }
 
 export default circleText;
