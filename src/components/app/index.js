fc42f1dadd1218f251fb38b0be0df083cf60d0c3
Jose <jose@runroom.com>
2020-12-11 10:32:16 +0100

    Removes old class code

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,6 +6,7 @@ import isExplorer from '@runroom/purejs/lib/isExplorer';
 import touchable from '@runroom/purejs/lib/touchable';
 import axios from 'axios';
 
+import useLoading from '../../hooks/useLoading';
 import AppRouter from '../appRouter';
 import Cursor from '../cursor';
 import Loader from '../loader';
@@ -16,51 +17,38 @@ import history from '../../utils/history'
 import GlobalStyle from '../../styles';
 
 const App = () => {
-  // state = {
-  //   loaded: false,
-  //   showLoader: false
-  // }
-
   axios.defaults.baseURL = playground;
   axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+  const { loading, showLoader } = useLoading();
 
   useEffect(() => {
     if (isExplorer()) document.documentElement.classList.add('browser-ie');
     document.documentElement.classList.add(touchable() ? 'touch' : 'non-touch');
-
-    // this.setState({ showLoader: true });
-
-    // setTimeout(() => {
-    //   this.setState({ loaded: true });
-    // }, 2400);
   }, []);
-
-  // render() {
-  //   const { loaded, showLoader } = this.state;
 
   return (
     <>
       <GlobalStyle />
-      {/* {loaded ? ( */}
+      {!loading ? (
         <ApolloProvider client={client}>
           <Cursor />
           <Router history={history}>
             <AppRouter />
           </Router>
         </ApolloProvider>
-       {/* ) : (
-           <TransitionGroup>
-             {showLoader && (
-               <CSSTransition classNames="loaded" timeout={2000}>
-                 <Loader />
-               </CSSTransition>
-             )}
-           </TransitionGroup>
-         )
-       } */}
+      ) : (
+          <TransitionGroup>
+            {showLoader && (
+              <CSSTransition classNames="loaded" timeout={2000}>
+                <Loader />
+              </CSSTransition>
+            )}
+          </TransitionGroup>
+        )
+      }
     </>
   );
-  // }
 }
 
 export default App;
